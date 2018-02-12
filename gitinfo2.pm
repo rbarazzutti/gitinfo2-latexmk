4337d17525fb898501294227fa9e1283e075e68f
Raphael P. Barazzutti <raphael.barazzutti@unine.ch>
2018-02-12 01:43:33 +0100

    works with sub-directories

--- gitinfo2.pm
+++ gitinfo2.pm
@@ -61,6 +61,13 @@ sub git_info_2 {
 
         # Hoover up the metadata
         my $metadata =`git --no-pager log -1 --date=short --decorate=short --pretty=format:"shash={%h}, lhash={%H}, authname={%an}, authemail={%ae}, authsdate={%ad}, authidate={%ai}, authudate={%at}, commname={%an}, commemail={%ae}, commsdate={%ad}, commidate={%ai}, commudate={%at}, refnames={%d}, firsttagdescribe={$FIRSTTAG}, reltag={$RELTAG} " HEAD`;
+        
+        # When running in a subfolder of the repo
+        my $dir = ".git";
+        if (!(-e $dir) and !(-d $dir)) {
+            mkdir($dir);
+        }
+
         open(my $fh,'>',$NGIN);
         print $fh "\\usepackage[".$metadata."]{gitexinfo}\n";
         close $fh;  
